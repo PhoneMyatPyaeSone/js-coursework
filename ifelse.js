fc3b3c69947aace22fba172fc033181fdd9fb424
phonemyat <phonemyatpyaesone8@gmail.com>
2021-06-10 22:37:03 +0630

    javascript coursework v1

---
+++ ifelse.js
@@ -0,0 +1,12 @@
+let food = "noodle";
+if(food === "coffee"){
+    console.log("coffee");
+}else if(food === "tea"){
+    console.log("tea")
+}else if(food === "shan noodle"){
+    console.log("shan noodle")
+}else if(food === "noodle"){
+    console.log("noodle")
+}else{
+    console.log("Not Found");
+}
---
+++ switch.js
@@ -0,0 +1,17 @@
+let food = "coffee";
+switch(food){
+    case "noodle":
+        console.log("noodle");
+    break;
+    case "apple":
+        console.log("apple");
+    break;
+    case "tea":
+        console.log("tea");
+    break;
+    case "coffee":
+        console.log("coffee");
+    break;
+    default:
+        console.log("undefined");
+}
